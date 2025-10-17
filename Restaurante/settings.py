24174371a66600496687a3d6d13bab85504df92f
DANILO BRUNO SAMPAIO CONCEICAO TEIXEIRA <danilo.conceicao@aluno.senac.rio>
2025-10-17 20:32:12 -0300

    Adiciona o middleware do WhiteNoise

--- Restaurante/settings.py
+++ Restaurante/settings.py
@@ -47,13 +47,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    MIDDLEWARE = 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # <--- ADICIONE ESTA LINHA AQUI
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'Restaurante.urls'
